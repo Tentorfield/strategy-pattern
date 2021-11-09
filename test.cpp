21279891591cc7935a27a7d05964014994b6068a
Harry64C <hcoop006@ucr.edu>
2021-11-08 21:54:40 -0800

    untracked Cmake files

--- test.cpp
+++
@@ -1,9 +0,0 @@
-#include "spreadsheet.hpp"
-#include "gtest/gtest.h"
-
-#include "contains_test.hpp"
-
-int main(int argc, char **argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
-}
