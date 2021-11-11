46a54fe767ccf670eda81d3966f62e642014f6a8
Harry64C <hcoop006@ucr.edu>
2021-11-11 12:42:23 -0800

    commiting changes

--- Select_Not.hpp
+++ Select_Not.hpp
@@ -1,16 +1,22 @@
+#ifndef __SELECT_NOT_HPP__
+#define __SELECT_NOT_HPP__
+
 #include "select.hpp"
 
-class Select_Not : public Select { // subclass of Strategy "Select"
-    private:
-	int column;
-	string name;
-	Spreadsheet* sheet;
+class Select_Not: public Select {
+   private:
+        Select* selection;
 
-    public: 
+   public:
+
 	// is passed in a new Select_Contains object
-	Select_Not(Select_Contains*);
+        Select_Not(Select* selection);
 
-        // Return true if the specified row should be selected.
-        bool select(const Spreadsheet* sheet, int row);
+        ~Select_Not();
+
+        // Return false if the specified row should be selected.
+        virtual bool select(const Spreadsheet* sheet, int row) const;
 
 };
+
+#endif //__SELECT_NOT_HPP__ 
